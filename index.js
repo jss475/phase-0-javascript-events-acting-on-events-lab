0691bcad2666de16fb30c4ef6b9df41275eb6172
JosephShin <joespam9999@gmail.com>
2022-04-10 23:05:34 -0400

    commit tree b7e9f26d22026f940992251dab513ba9f2e5be73

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function moveDodgerLeft(){
+    const test = dodger.style.left
+    const leftNumbers = dodger.style.left.replace("px","");
+    //debugger
+    const left = parseInt(leftNumbers,10)
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+      }
+}
+
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px","")
+    const right = parseInt(rightNumbers,10)
+
+    if (right < 360 && right > 0) {
+        dodger.style.left = `${right + 10}px`
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if((e.key === "ArrowLeft")){
+        moveDodgerLeft()
+    }else if(e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+    }
+
+)
